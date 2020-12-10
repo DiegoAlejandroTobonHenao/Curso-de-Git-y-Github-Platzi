8975d320e748b83c5d7b5d2a9a0b6aa9d43e28c2
Diego Alejandro Tobón Henao <diego.1701818111@ucaldas.edu.co>
2020-12-10 14:36:31 -0500

    Programa que valida la edad

--- golang1.go
+++ golang1.go
@@ -3,6 +3,16 @@ package main
 import ("fmt")
 
 func main(){
+
+	var nombre string = "Diego Alejandro Tobon"
+	var edad int = 20
+
 	fmt.Println("Hola Mundo")
+	fmt.Println("Bienvenido ",nombre)
+	if (edad >= 18){
+		fmt.Println("Mayor de edad")
+	} else{
+		fmt.Println("No puedes ... :( ")
+	}
 }
 
